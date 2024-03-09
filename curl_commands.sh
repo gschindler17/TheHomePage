f6c19c82cc013d5e86c747327e8fb9d8aa24d4ca
Garrett Schindler <garrettschindler17@gmail.com>
2024-03-08 21:57:14 -0600

    Changing background, waiting to add white

--- curl_commands.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST "https://accounts.spotify.com/api/token" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id=754792fb513a4758af5ea3b0729020b0&client_secret=551fe853ff204494ad79e3bc613e79dd"
